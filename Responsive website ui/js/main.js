17cfbf8b4bb33e6d1d4386728f748b64e5f8928a
JABBARI <yasseelle8@gmail.com>
2020-07-13 03:03:29 +0100

    comit

--- Responsive website ui/js/main.js
+++ Responsive website ui/js/main.js
@@ -1,26 +1,28 @@
-$(document).ready(function(){
-    /* navigation bar and hunberger menu functionamity started here*/
-    $(".nav-icon").click(function()
-    {
-        $(".full-nav").addClass("open");
-    });
-    $(".nav-close").click(function()
-    {
-        $(".full-nav").removeClass("open");
-    });
+$(document).ready(function () {
+  /* navigation bar and hunberger menu functionamity started here*/
+  $(".nav-icon").click(function () {
+    $(".full-nav").addClass("open");
+  });
+  $(".nav-close").click(function () {
+    $(".full-nav").removeClass("open");
+  });
 
-    $(window).scroll(function()
-    {
-           var sc = $(window).scrollTop();
-           if(sc > 100)
-           {
-               $(".nav").addClass("sticky");
-           } 
-          else
-           {
-               $(".nav").removeClass("sticky");
-           } 
-    });
-        /* navigation bar and hunberger menu functionamity finished here*/
-
+  $(window).scroll(function () {
+    var sc = $(window).scrollTop();
+    if (sc > 100) {
+      $(".nav").addClass("sticky");
+    } else {
+      $(".nav").removeClass("sticky");
+    }
+  });
+  /* navigation bar and hunberger menu functionamity finished here*/
+  $(".bxslider").bxSlider({
+    mode: "horizontal",
+    moveSlides: 1,
+    slideMargin: 40,
+    infiniteLoop: true,
+    minSlides: 1,
+    maxSlides: 1,
+    speed: 1200,
+  });
 });
